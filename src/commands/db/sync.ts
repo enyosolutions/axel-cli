d634134c63d71abdfb1c42bfa4cd58a61a875fa4
fb@enyosolutions.com <fb@enyosolutions.com>
2020-10-27 10:02:32 +0000

    fix: importing index.mjs files

--- src/commands/db/sync.ts
+++ src/commands/db/sync.ts
@@ -28,14 +28,14 @@ export default class Sync extends Command {
 
   async run() {
     const {flags} = this.parse(Sync)
-    this.log('Running db:sync based on models.')
-    if (!fs.existsSync(path.resolve(process.cwd(), 'src/resources/sequelize/models'))) {
+
+    const resource = path.resolve(process.cwd(), 'src/resources/sequelize/models/index.mjs')
+    this.log('Running db:sync based on models in ', resource)
+    if (!fs.existsSync(resource)) {
       this.error('models resources not found ! . Please make sure you compile your code before running axel')
       return;
     }
-    const dbImportConfig = import(
-      path.resolve(process.cwd(), 'src/resources/sequelize/models')
-    )
+
     const alter = flags.alter
     const force = flags.force
     const silent = flags.silent
@@ -49,7 +49,7 @@ export default class Sync extends Command {
         return
       }
     }
-    await dbImportConfig
+    import(resource)
     .then(db => {
       return db.default.sequelize.sync({alter, force})
     })
